9c1eb19e2f1ebb10352e57d7537815dac12dd62b
11256025-cpu <11256025@ntub.edu.tw>
2026-01-16 20:42:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-from flask import render_template
+# app.py
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/page/<name>")
-def home(name):
-    return render_template("index.html", name = name)
+@app.route('/')
+def index():
+    # 假設我們從資料夾渲染 index.html
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug = True)
+@app.route('/submit', methods=['POST'])
+def submit():
+    # 這裡處理表單提交的邏輯
+    username = request.form.get('username') # 讀取HTTP request
+    return f"感謝註冊，{username}！"
+
+if __name__ == '__main__':
+    app.run(debug=True)
